c5b48096f5b2d935ecb1b024d3b756a3763fa4b9
Jomo Fisher <jomofisher@gmail.com>
2024-11-22 22:11:19 +0000

    Bootstrap cmake works

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
-LLVM_BRANCH=llvm-project
+LLVM_BRANCH=llvm
+#rf -rf build-$LLVM_BRANCH
 mkdir build-$LLVM_BRANCH
 cd build-$LLVM_BRANCH
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" ../external/$LLVM_BRANCH/llvm
+cmake -G Ninja -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" ../external/$LLVM_BRANCH/llvm
+ninja
